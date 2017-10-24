22d77d28112da92bb8b7914d6942f8b14097bf06
antonio <tmowna@gmail.com>
2017-10-24 20:45:01 -0200

    Release 0.0.28

--- src/directives/brmasker4.ts
+++ src/directives/brmasker4.ts
@@ -46,9 +46,6 @@ export class BrMaskerDirective {
   private moneyMask(v: any): string {
     let tmp = v;
     tmp = tmp.replace(/([0-9]{2})$/g, ',$1');
-    if ( tmp.length > 6 ) {
-      tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, '.$1,$2');
-    }
     return tmp;
   }
   private onInput(value: any): void {
